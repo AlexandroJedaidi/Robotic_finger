9cfc39aa89018d29f2971b94ebd5d3c470474a6e
daniellarin22 <larindaniel22@gmail.com>
2023-05-18 14:10:02 +0200

    Add files via upload

--- elastic_beam.py
+++ elastic_beam.py
@@ -26,12 +26,17 @@ from dolfinx.fem.petsc import assemble_matrix, assemble_vector		# https://docs.f
 from dolfinx.fem import assemble_scalar, locate_dofs_topological, dirichletbc
 from dolfinx.mesh import locate_entities_boundary, create_interval
 from ufl import (SpatialCoordinate, inner, TestFunctions, TrialFunctions, nabla_grad, nabla_div, dx, MixedElement, TestFunction, TrialFunction)
+from scipy.optimize import fsolve
 
 from scipy.integrate import solve_ivp
 
 
 NUM_ELEMENTS = 10
+NUM_TIME_STEPS = 100
 T = 1.0
+
+
+def tau(t): return 0.1 * np.sin(t)
 
 
 L = 1.1               # beam length
@@ -68,8 +73,8 @@ v_element = basix.ufl_wrapper.create_element(
 u_function_space = FunctionSpace(mesh, u_element)           # u_function_space.dofmap.list
 v_function_space = FunctionSpace(mesh, v_element)
 
-u = TrialFunction(u_function_space)
-v = TrialFunction(v_function_space)
+u_ = TrialFunction(u_function_space)
+v_ = TrialFunction(v_function_space)
 w = TestFunction(u_function_space)
 z = TestFunction(v_function_space)
 
@@ -78,7 +83,7 @@ z = TestFunction(v_function_space)
 # embed()
 
 left_boundary = locate_entities_boundary(
-    mesh, mesh.topology.dim-1, lambda x: np.isclose(x[0], 0)
+    mesh, mesh.topology.dim-1, lambda s: np.isclose(s[0], 0)
 )
 u_boundary_dofs = locate_dofs_topological(u_function_space, mesh.topology.dim-1, left_boundary)
 v_boundary_dofs = locate_dofs_topological(v_function_space, mesh.topology.dim-1, left_boundary)
@@ -102,15 +107,15 @@ def d2_dx2(f): return nabla_div(nabla_grad(f))
 
 
 system_matrices = {
-    "M_u": rho * A * u * w * dx,
-    "M_v": rho * A * v * z * dx,
-    "C_uv": rho * A * u * z * dx,
-    "C_vu": rho * A * v * w * dx,
-    "K_u": E * A * inner(d_dx(u), d_dx(w)) * dx,
-    "K_v": E * I * d2_dx2(v) * d2_dx2(z) * dx,
-    "K_u1": rho * A * u * w * dx,
-    "K_v1": rho * A * v * z * dx,
-    "K_v2": rho * A * (R * (L - x[0]) + 0.5 * (L + x[0]) * (L - x[0])) * inner(d_dx(v), d_dx(z)) * dx,
+    "M_u": rho * A * u_ * w * dx,
+    "M_v": rho * A * v_ * z * dx,
+    "C_uv": rho * A * u_ * z * dx,
+    "C_vu": rho * A * v_ * w * dx,
+    "K_u": E * A * inner(d_dx(u_), d_dx(w)) * dx,
+    "K_v": E * I * d2_dx2(v_) * d2_dx2(z) * dx,
+    "K_u1": rho * A * u_ * w * dx,
+    "K_v1": rho * A * v_ * z * dx,
+    "K_v2": rho * A * (R * (L - x[0]) + 0.5 * (L + x[0]) * (L - x[0])) * inner(d_dx(v_), d_dx(z)) * dx,
 }
 system_vectors = {
     "K_u2": rho * A * (R + x[0]) * w * dx,
@@ -144,20 +149,15 @@ built_scalars = {name: assemble_scalar(form(scal)) for name, scal in system_scal
 
 
 # TODO: use petsc4py.PETSc .Mat and .Vec instead of np arrays
-def ode(t, y, u_dim, v_dim, args, tau):
+def ode_lhs(t, y, args):
+    u_dim, v_dim = args["u_dim"], args["v_dim"]
+
     u = y[0 : u_dim]
     v = y[u_dim : u_dim+v_dim]
-    theta = y[u_dim+v_dim]
-    p = y[u_dim+v_dim+1 : 2*u_dim+v_dim+1]
-    q = y[2*u_dim+v_dim+1 : 2*(u_dim+v_dim)+1]
-    alpha = y[-1]
-
-    # print(u.shape, v.shape, p.shape, q.shape, theta.shape, alpha.shape, (args["C_vu"].dot(u) + args["C_v"]).shape)
-
-    # TODO: after 3 stesp we get shape mismatch i.e. u.shape is 11,68 instead of 11, somewhere in m_alpha
-    ## check every reshape here and reduce it to lowest neccesary number
-    from IPython import embed
-    embed()
+    # theta = y[u_dim+v_dim]
+    # p = y[u_dim+v_dim+1 : 2*u_dim+v_dim+1]
+    # q = y[2*u_dim+v_dim+1 : 2*(u_dim+v_dim)+1]
+    # alpha = y[-1]
 
     m_alpha = np.array(
         args["I_b"] + u.T.dot(args["K_u1"]).dot(u) + 2 * args["K_u2"].dot(u) + v.T.dot(args["K_v1"] - args["K_v2"]).dot(v)
@@ -172,7 +172,7 @@ def ode(t, y, u_dim, v_dim, args, tau):
         [
             np.zeros((v_dim,u_dim)),
             args["M_v"],
-            args["C_vu"].dot(u) + args["C_v"].reshape(v_dim, 1)
+            (args["C_vu"].dot(u) + args["C_v"]).reshape(v_dim, 1)
         ],
         [
             (-v.T.dot(args["C_vu"])).reshape(1, u_dim),
@@ -185,33 +185,74 @@ def ode(t, y, u_dim, v_dim, args, tau):
     mass_matrix = np.block([
         [np.eye(dim), np.zeros((dim, dim))],
         [np.zeros((dim, dim)), mass_matrix_lower_right_block]
-    ])
+    ]).reshape((y.shape[0], y.shape[0]))
+
+    return mass_matrix
+
+
+def ode_rhs(t, y, args):
+    u_dim, v_dim = args["u_dim"], args["v_dim"]
+
+    u = y[0: u_dim]
+    v = y[u_dim: u_dim + v_dim]
+    # theta = y[u_dim + v_dim]
+    p = y[u_dim + v_dim + 1: 2 * u_dim + v_dim + 1]
+    q = y[2 * u_dim + v_dim + 1: 2 * (u_dim + v_dim) + 1]
+    alpha = y[-1]
 
     k_alpha = 2 * p.T.dot(args["K_u1"]).dot(u) + 2 * args["K_u2"].dot(p) + 2 * q.T.dot(args["K_v1"] - args["K_v2"]).dot(v)
 
     right_hand_side_lower_block = np.block([
         [
-            2 * theta * args["C_uv"].dot(q) - (args["K_u"] - theta**2 * args["K_u1"]).dot(u) + theta**2 * args["K_u2"].reshape(u_dim, 1)
+            (2 * alpha * args["C_uv"].dot(q) - (args["K_u"] - alpha**2 * args["K_u1"]).dot(u) + alpha**2 * args["K_u2"]).reshape(u_dim, 1)
         ],
         [
-            (-2 * theta * args["C_vu"].dot(p) - (args["K_v"] - theta**2 * args["K_v1"] + theta**2 * args["K_v2"]).dot(v)).reshape(v_dim, 1)
+            (-2 * alpha * args["C_vu"].dot(p) - (args["K_v"] - alpha**2 * args["K_v1"] + alpha**2 * args["K_v2"]).dot(v)).reshape(v_dim, 1)
         ],
         [
-            np.array(tau - k_alpha * theta + p.T.dot(args["C_uv"]).dot(q) + q.T.dot(args["C_vu"]).dot(p))
+            np.array(args["tau"](t) - k_alpha * alpha + p.T.dot(args["C_uv"]).dot(q) + q.T.dot(args["C_vu"]).dot(p))
         ],
     ])
 
+    dim = u_dim + v_dim + 1
     right_hand_side = np.block([
         [y[dim :].reshape(dim, 1)],
         [right_hand_side_lower_block]
-    ])
+    ]).reshape((y.shape[0],))
 
-    return np.linalg.solve(mass_matrix, right_hand_side).reshape(-1,)
+    return right_hand_side
+
+
+def implicit_midpoint_method(fun_lhs, fun_rhs, t_span, steps, y0, args):
+    dim = y0.shape[0]
+    y = np.zeros((steps, dim))
+    y[0] = y_initial
+
+    step_size = (t_span[1] - t_span[0]) / steps
+    t_n = 0
+    for n in range(steps-1):
+        y[n+1] = fsolve(
+            lambda x:
+            fun_lhs(
+                t_n + step_size / 2,
+                ((y[n] + x) / 2).reshape((dim,)),
+                args
+            ).dot(((x - y[n]).reshape((dim,)))) -
+            step_size * fun_rhs(
+                t_n + step_size / 2,
+                ((y[n] + x) / 2).reshape((dim,)),
+                args
+            ),
+            y[0]
+        )
+        t_n += step_size
+
+    return y
 
 
 dimu = built_np_matrices["M_u"].shape[0]
 dimv = built_np_matrices["M_v"].shape[0]
-args_mat = built_np_matrices | built_np_vectors | built_scalars
+args_mat = built_np_matrices | built_np_vectors | built_scalars | {"u_dim": dimu, "v_dim": dimv, "tau": tau}
 
 y_initial = np.block([
     [np.zeros((dimu + dimv, 1))],
@@ -219,22 +260,42 @@ y_initial = np.block([
     [np.zeros((dimu + dimv, 1))],
     [np.array(alpha_initial).reshape(1, 1)],
 ]).reshape(-1,)
-tau_test = 0
 
-y_solution = solve_ivp(
-    fun=ode,
-    t_span=(0, T),
-    y0=y_initial,
-    method="BDF",    # Radau, LSODA, implicit midpoint rule (symplectic)
-    vectorized=True,
-    args=(dimu, dimv, args_mat, tau_test)
+y_solution = implicit_midpoint_method(
+    ode_lhs,
+    ode_rhs,
+    (0, T),
+    NUM_TIME_STEPS,
+    y_initial,
+    args=args_mat
 )
+
+u_sol = y_solution[:, dimu+dimv+1 : 2*dimu+dimv+1]
+v_sol = y_solution[:, 2*dimu+dimv+1 : 2*(dimu+dimv)+1]
+theta_sol = y_solution[:, -1]
+
+
+
+
+# https://docs.fenicsproject.org/dolfinx/main/python/generated/dolfinx.fem.html
+# https://docs.fenicsproject.org/dolfinx/main/python/generated/dolfinx.cpp.common.html
+
+u_1 = Function(u_function_space)
+u_1.x.array[:] = u_sol[1]
+
+v_1 = Function(v_function_space)
+v_1.x.array[:] = v_sol[1]
 
 from IPython import embed
 embed()
 
+v_index_x = v_function_space.tabulate_dof_coordinates() #[:,0]
+v_1.eval(v_index_x, v_1)
 
-u_sol, v_sol = y_solution[dimu+dimv+1 : 2*dimu+dimv+1],  y_solution[2*dimu+dimv+1 : 2*(dimu+dimv)+1]
+# v_function_space.dofmap.list
+
+
+
 
 # TODO: check how to interpolate array over whole function_space to get Function() object
 ## or get all spatial coordinates
